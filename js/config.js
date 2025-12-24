2098ad31a12ab757e05028c935558812f4c9b4a7
Nitin <119484935+nitingit7@users.noreply.github.com>
2025-12-24 16:32:29 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -27,7 +27,8 @@ var GITHUB_FILES = {
     // New Topic Tests Category
     'quant_topic': [
         { name: "Number System 1", tags: "Number System", file: BASE_URL + "tests/quant/TopicTest/Number System/QANS001.json" },
-        { name: "Number System 2", tags: "Number System", file: BASE_URL + "tests/quant/TopicTest/Number System/QANS002.json" }
+        { name: "Number System 2", tags: "Number System", file: BASE_URL + "tests/quant/TopicTest/Number System/QANS002.json" },
+        { name: "Algebra 1", tags: "Algebra", file: BASE_URL + "tests/quant/TopicTest/Algebra/QAA001.json" }
     ],
 
     // --- 2. REASONING ABILITY ---
